d827d776c91d85518bd6ef94f4799f4150d40d4f
14ypu <101351621+14ypu@users.noreply.github.com>
2023-09-03 10:16:35 +0700

    plot bar for ignition delay // plot marker for preCombustionpress

--- ignitiondelay/maximunCombustionpressure.m
+++ ignitiondelay/maximunCombustionpressure.m
@@ -1,94 +1,83 @@
 close all;
-fig2 = figure('Position',[0 0 1300 850]);
-%input parameter
+fig = figure('Position',[0 0 1300 850]);
 maxCombustionPress800_800 = 12.57;
-maxCombustionPress800_1200 = 10.75;
+maxCombustionPress800_1200 = 10.57;
 maxCombustionPress1000_800 = 9.86;
 maxCombustionPress1000_1200 = 11.58;
-maxCombustionPress = [maxCombustionPress800_800;maxCombustionPress800_1200;maxCombustionPress1000_800;maxCombustionPress1000_1200];
-errhigh = [1 ;1; 1; 1];
-errlow = [1; 2 ;1; 1];
 
-%err = 0.3*y;
-%y = [1200K 1000K 800K];
-%x = categorical({'1200K','1000K','800K'});
-%x = reordercats(x,{'1200K','1000K','800K'});
-%ylim([0 5]);
-%bp = bar(x,y,'BarWidth',1); %Acceptable range [0 1] 
-bp = bar(maxCombustionPress,'BarWidth',0.7,'LineWidth',1.5); %Acceptable range [0 1] 
+maxCombustionPress = [maxCombustionPress800_800 maxCombustionPress800_1200 maxCombustionPress1000_800 maxCombustionPress1000_1200];
+%Oxy15 = [1.6 2.256 3.184];
+%Oxy10 = [1.87 3 3.616];
+%ytotal = [maxCombustionPress; Oxy15; Oxy10];
+errhigh = [0.11 0.11 0.11 0.1];
+errlow = [0.15 0.15 0.15 0.15];
+%err15 = [0.1 0.12 0.17];
+%err10 = [0.14 0.11 0.18];
+x = [1 2 3 4];
+
+
+plot(x,maxCombustionPress,'square',MarkerFaceColor=[0 0 0],MarkerSize=12,MarkerEdgeColor='none');
 hold on
-for k = 1:size(maxCombustionPress,2)
-    % get x positions per group
-    xpos = bp(k).XData + bp(k).XOffset;
-    % draw errorbar
-    errorbar(xpos, maxCombustionPress(:,k), errlow(:,k),errhigh(:,k), 'LineStyle', 'none', ... 
-        'Color', 'k', 'LineWidth', 1.5);
-end
-%title('What is my name?','FontSize',20,'Position',[2 4.7])
-xlabel('Ambient temperature (K)','FontSize',20,'FontName','times');
-ylabel('Maximum Combustion Pressure (ms)','FontSize',20,'FontName','times');
-%{
-hp = bar(y);
-cm = colororder; % or replace with the desired colormap
-hatchfill2(hp(1),'single','HatchAngle',0,'hatchcolor',cm(1,:));
-hatchfill2(hp(2),'cross','HatchAngle',45,'hatchcolor',cm(2,:));
-hatchfill2(hp(3),'single','HatchAngle',45,'hatchcolor',cm(3,:));
-for i = 1:numel(hp)
-    hp(i).FaceColor = 'none';
-end
-%}
-cm = colororder; % or replace with the desired colormap
-A = hatchfill2(bp(1),'single','HatchAngle',0,'hatchcolor',[0 0 0],'HatchLineWidth',1.5);
-B = hatchfill2(bp(2),'cross','HatchAngle',45,'hatchcolor',[1 0 0],'HatchLineWidth',1.5);
-C = hatchfill2(bp(3),'single','HatchAngle',45,'hatchcolor',[0 0 1],'HatchLineWidth',1.5);
-D = hatchfill2(bp(4),'single','HatchAngle',45,'hatchcolor',[0 0 1],'HatchLineWidth',1.5);
 
-for i = 1:numel(bp)
-    bp(i).FaceColor = 'none';
-end
+errorbar(x,maxCombustionPress,errhigh,errlow,"vertical","LineStyle","none",Color=[0 0 0],LineWidth=1);
 hold on
-%bp(1).FaceColor = 'r';
-%bp(2).FaceColor = 'g';
-%bp(3).FaceColor = 'b';
-%hatchfill2(legendABC(1),'single','HatchAngle',0,'hatchcolor',[0 0 0],'HatchLineWidth',1.5);
 
-%legend([A B C D],{' 21% O_{2} ' ' 15% O_{2} ' ' 10% O_{2} '},'FontSize',20,'FontWeight','normal');
+x_loc = 1:length(maxCombustionPress);
+y_loc = maxCombustionPress + 0.05;
+text(x_loc+0.1,y_loc,num2str(maxCombustionPress'),'vert','top','horiz','left','FontSize',20,'Fontname','Times'); 
+hold on
+%plot(x,Oxy15,'square',MarkerFaceColor=[1 0 0],MarkerSize=14,MarkerEdgeColor='none');
+%hold onx`
+%
+%errorbar(x,Oxy15,err15,"vertical","LineStyle","none",Color=[0 0 0],LineWidth=1);
+%hold on
+%
+%plot(x,Oxy10,'o',MarkerFaceColor=[0 0 1],MarkerSize=12,MarkerEdgeColor='none');
+%hold on
+%
+%errorbar(x,Oxy10,err10,"vertical","LineStyle","none",Color=[0 0 0],LineWidth=1);
+%hold on
 
-%set(gca,'YTick',0:1:4,'YTickLabel',0:1:5)
-set(gca,'YLim',[4 16]);
-set(gca,'YTick',4:2:16);
-set(gca, 'XTickLabel', {'1200','1000','800','zz'},'FontName','times');
+%coefficients21 = polyfit(x, maxCombustionPress, 1);
+%xFit = linspace(min(x), max(x), 1000);
+%yFit21 = polyval(coefficients21 , xFit);
+%%plot(xFit, yFit,'LineWidth', 1.25,'LineStyle','--');
+%plot(xFit, yFit21, 'LineWidth', 1.5,'LineStyle','--','Color',[0 0 0]);
+%hold on
+%
+%coefficients15 = polyfit(x, Oxy15, 1);
+%xFit = linspace(min(x), max(x), 1000);
+%yFit15 = polyval(coefficients15 , xFit);
+%hold on;
+%%plot(xFit, yFit,'LineWidth', 1.25,'LineStyle','--');
+%plot(xFit, yFit15, 'LineWidth', 1.5,'LineStyle','--','Color',[1 0 0]);
+%hold on
+%
+%coefficients10 = polyfit(x, Oxy10, 1);
+%%xFit = linspace(min(x), max(x), 1000);
+%yFit10 = polyval(coefficients10 , xFit);
+%hold on;
+%%plot(xFit, yFit,'LineWidth', 1.25,'LineStyle','--');
+%plot(xFit, yFit10, 'LineWidth', 1.5,'LineStyle','--','Color',[0 0 1]);
+%hold on
 ax = gca;
-ax.XAxis.FontSize = 20; %Xtick fontsize %https://www.mathworks.com/matlabcentral/answers/511665-fontsize-and-properties-of-xticklabels-using-figure-handles
-ax.YAxis.FontSize = 20; %Ytick fontsize
+
+xlim([0.5 4.5]);
+xticks(1:1:4);
+xlabel('Ambient temperature condition (K)','FontSize',20,'Color','black'); % left y-axis 
+set(gca, 'XTickLabel', {'800K-800bar','800K-1200bar','1000K-800bar','1000K-1200bar'},'FontSize',20);
+
+ylim([9 13.5]);
+yticks(9:0.5: 13.5);
+ylabel('Maximum Compression Pressure (bar)','FontSize',20,'Color','black'); % left y-axis 
+
 set(gca,'fontname','Times','FontSize',20); % Set it to times
+set(gca,'LineWidth',2);
+%legend({'21% O_{2} ','','15% O_{2} ','','10% O_{2} ',''},'FontSize',20,'Fontweight','normal','Orientation','horizontal',LineWidth=1.5);
+str = {'Diesel combustion';'Solenoid injector';'O_{2} = 21 %'};
+text(3.5,12.75,str,'FontName','Times','FontSize',20,'FontWeight','normal',BackgroundColor=[1 1 1]);
 
-%text(,num2str(y(:,1)),'HorizontalAlignment','center','VerticalAlignment','bottom');
-%errhigh = 0.1*y;
-%errlow  = (-0.1)*y;
-%er = errorbar(x,y,errlow,errhigh);   
-
-%xCnt = vertcat(bp.XEndPoints);
-%text(xCnt(:),y(:),compose('%d',y(:)),'HorizontalAlignment','center','VerticalAlignment','bottom');
-%text(1:length(y),y,num2str(y),'vert','bottom','horiz','center')
-
-%width = bp.BarWidth;
-%for i=1:length(y(:, 1))
-%    row = y(i, :);
-%    % 0.5 is approximate net width of white spacings per group
-%    offset = ((width + 0.3) / length(row)) / 2;
-%    x = linspace(i-offset, i+offset, length(row));
-%    text(x,row,num2str(row'),'vert','bottom','horiz','center','FontSize',18,'FontWeight');
-%end
-%str = {'P_{inj} = 1000bar';'m_{inj} = 67.93mg'};
-%text(0.9,20,str,'FontSize',16,'FontWeight','normal',BackgroundColor=[1 1 1]);
-
-%set(gca,'fontname','Carial','FontSize',20); % Set it to times
-set(gca,'LineWidth',1.5);
-set(gca,'fontweight');
-
-%str = {'Diesel combustion';'Solenoid injector';'P_{inj} = 1000 bar'};
-%text(1.1,14,str,'FontSize',20,'FontWeight','normal',BackgroundColor=[1 1 1],fontname='times');
-
-%grid on
+%ax.ZGrid = 'on';
+ax.XGrid = 'off';
+ax.YGrid = 'off';
 hold on
